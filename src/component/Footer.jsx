0877bca7054c011ca27d985cab95686ad67ace5e
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-16 20:52:10 +0100

    desgned footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-blue-950 text-white text-xs mt-[6rem] h-14  block text-center'>
+    <div className='bg-blue-950 text-white text-xs mt-[6rem] h-14   block text-center'>
     <p className='pt-[1.5rem]'>&copy; Lock Lingo 2023 Adesuwa</p>
  </div>
    
